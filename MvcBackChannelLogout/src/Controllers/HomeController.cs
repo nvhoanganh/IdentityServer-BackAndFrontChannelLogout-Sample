63e9d69503a46720b95bcb625b97eb91cb6ee974
Anthony Nguyen <nvhoanganh1909@gmail.com>
2021-06-07 17:55:06 +1000

    intial commit

---
+++ MvcBackChannelLogout/src/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HomeController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IActionResult Index() => View();
+
+        [Authorize]
+        public IActionResult Secure() => View();
+
+        public IActionResult Logout()
+        {
+            Console.WriteLine("Received logout request from user");
+            return SignOut("oidc");
+        }
+    }
+}
---
+++ MvcBackChannelLogout2/src/CookieEventHandler.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using System;
+
+namespace Client
+{
+    public class CookieEventHandler : CookieAuthenticationEvents
+    {
+        public CookieEventHandler(LogoutSessionManager logoutSessions)
+        {
+            LogoutSessions = logoutSessions;
+        }
+
+        public LogoutSessionManager LogoutSessions { get; }
+
+        public override async Task ValidatePrincipal(CookieValidatePrincipalContext context)
+        {
+            Console.WriteLine($">>>>>> Checking if user is authenticated");
+            if (context.Principal.Identity.IsAuthenticated)
+            {
+                Console.WriteLine($">>>>>> user has valid cookie");
+                var sub = context.Principal.FindFirst("sub")?.Value;
+                var sid = context.Principal.FindFirst("sid")?.Value;
+
+                if (LogoutSessions.IsLoggedOut(sub, sid))
+                {
+                    Console.WriteLine($">>>>>> but this cookie is logged out from Identity Server. Killing the cookie");
+                    context.RejectPrincipal();
+                    await context.HttpContext.SignOutAsync();
+
+                    // todo: if we have a refresh token, it should be revoked here.
+                }
+            }
+        }
+    }
+}
