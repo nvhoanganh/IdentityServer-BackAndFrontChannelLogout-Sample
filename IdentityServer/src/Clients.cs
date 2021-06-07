806206cd51b8bc7cb716bba5a719a62af97e3d2c
Anthony Nguyen <nvhoanganh1909@gmail.com>
2021-06-07 18:12:50 +1000

    Add sampe of front channel

--- IdentityServer/src/Clients.cs
+++ IdentityServer/src/Clients.cs
@@ -116,9 +116,39 @@ namespace IdentityServerHost
                     
                     AllowedGrantTypes = GrantTypes.Code,
 
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+                    RedirectUris = { "https://localhost:44302/signin-oidc" },
+                    FrontChannelLogoutUri = "https://localhost:44302/signout-oidc",
+                    PostLogoutRedirectUris = { "https://localhost:44302/signout-callback-oidc" },
+
+                    AllowOfflineAccess = true,
+                    AllowedScopes = { "openid", "profile", "scope1", "scope2" }
+                },
+                new Client
+                {
+                    ClientId = "interactive.mvc.sample.jarjwt2",
+                    
+                    // force client to use signed authorize request
+                    RequireRequestObject = true,
+                    
+                    // this client uses an RSA key as client secret
+                    // this key is used for both validating the signature on the authorize request
+                    // and for client authentication
+                    // see https://docs.duendesoftware.com/identityserver/v5/advanced/jar/
+                    // and https://docs.duendesoftware.com/identityserver/v5/tokens/authentication/jwt/
+                    ClientSecrets =
+                    {
+                        new Secret
+                        {
+                            Type = IdentityServerConstants.SecretTypes.JsonWebKey,
+                            Value = "{'e':'AQAB','kid':'ZzAjSnraU3bkWGnnAqLapYGpTyNfLbjbzgAPbbW2GEA','kty':'RSA','n':'wWwQFtSzeRjjerpEM5Rmqz_DsNaZ9S1Bw6UbZkDLowuuTCjBWUax0vBMMxdy6XjEEK4Oq9lKMvx9JzjmeJf1knoqSNrox3Ka0rnxXpNAz6sATvme8p9mTXyp0cX4lF4U2J54xa2_S9NF5QWvpXvBeC4GAJx7QaSw4zrUkrc6XyaAiFnLhQEwKJCwUw4NOqIuYvYp_IXhw-5Ti_icDlZS-282PcccnBeOcX7vc21pozibIdmZJKqXNsL1Ibx5Nkx1F1jLnekJAmdaACDjYRLL_6n3W4wUp19UvzB1lGtXcJKLLkqB6YDiZNu16OSiSprfmrRXvYmvD8m6Fnl5aetgKw'}"
+                        }
+                    },
+                    
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    RedirectUris = { "https://localhost:44303/signin-oidc" },
+                    FrontChannelLogoutUri = "https://localhost:44303/signout-oidc",
+                    PostLogoutRedirectUris = { "https://localhost:44303/signout-callback-oidc" },
 
                     AllowOfflineAccess = true,
                     AllowedScopes = { "openid", "profile", "scope1", "scope2" }
--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # Demo of how backchannel logout works with DuendeIdentity Server
 
-- run all 3 projects at the same time by using `dotnet run` in the correct folder
+- run `IdentityServer`, `MvcBackChannelLogout` and `MvcBackChannelLogout2` projects at the same time by using `dotnet run` in the correct folder
 - open `https://localhost:44300` and `https://localhost:44301`
 - login from `https://localhost:44300` by click on `Secure` tab, use `alice` and `alice` as username and password
 - then on `https://localhost:44301` , click on `Secure` tab again -> you will login straightaway
 - then `https://localhost:44300` click on log out -> you see the logged out confirmation page on IDS
 - on `https://localhost:44301`, reload the page -> you see tht you get kicked out as well
+
+# Demo of how front-channel logout works with DuendeIdentity Server
+
+- run `IdentityServer`, `MvcJarJwt` and `MvcJarJwt2` projects at the same time by using `dotnet run` in the correct folder
+- open `https://localhost:44302` and `https://localhost:44303`
+- login from `https://localhost:44302` by click on `Secure` tab, use `alice` and `alice` as username and password
+- then on `https://localhost:44303` , click on `Secure` tab again -> you will login straightaway
+- then `https://localhost:44302` click on log out -> you see the logged out confirmation page on IDS
+- on `https://localhost:44303`, reload the page -> you see tht you get kicked out as well
+- come back to the 1st tab (where you see "you have logged out" page from IDS), inspect element => you should see an Iframe with 2 links back to https://localhost:44302 and https://localhost:44303
